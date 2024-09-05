0043b661dc7fd1f4e57eac4ab7750649300cae35
verenarath <verenarath@edu.aau.at>
2024-09-05 20:13:31 +0200

    added styles

--- Events.php
+++ Events.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <?php include 'header.php'; ?>
+    <link rel="stylesheet" href="styles.css">
 </head>
 <body>
 <?php include 'apiCall.php'; ?>
